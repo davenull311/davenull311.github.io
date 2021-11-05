d8ad19ff66469c9446908024535d319cddf62d2a
Dave <davenull311@gmail.com>
2021-11-04 22:01:26 -0400

    Update puzzle app

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -2,16 +2,15 @@
 let VIDEO = null;
 let CANVAS = null;
 let CONTEXT = null;
-let SCALER = 0.8;
-let SIZE = {x: 0, y: 0, width: 0, height: 0};
+let SCALER = 0.9;       // Sets percent of screen the image takes up
+let SIZE = {x: 0, y: 0, width: 0, height: 0};       // Set dynamically below - DO NOT ADJUST HERE
 
 
 function main(){
 // Initialize canvas and context, set canvas size
     CANVAS = document.getElementById("puzzleCanvas");
     CONTEXT = CANVAS.getContext("2d");
-    CANVAS.width = window.innerWidth;
-    CANVAS.height = window.innerHeight;
+    
 
 // Initialize camera, create function to play video from source
     let promise = navigator.mediaDevices.getUserMedia({video:true});
@@ -21,16 +20,8 @@ function main(){
         VIDEO.play();
 
         VIDEO.onloadeddata = function(){
-            let resizer = SCALER*
-                Math.min(
-                    window.innerWidth / VIDEO.videoWidth,
-                    window.innerHeight / VIDEO.videoHeight
-                );
-// Size the incoming image to an optimal size, centered on the canvas, with room to move pieces            
-            SIZE.width = resizer * VIDEO.videoWidth;
-            SIZE.height = resizer * VIDEO.videoHeight;
-            SIZE.x = window.innerWidth/2 - SIZE.width/2;
-            SIZE.y = window.innerHeight/2 - SIZE.height/2;
+            handleResize();
+            window.addEventListener('resize',handleResize);
             updateCanvas();
         }
 
@@ -39,6 +30,23 @@ function main(){
         alert("Camera error: " +err);
     });
     
+}
+
+function handleResize(){
+// Automatically resize the window and orientation of the canvas with change to the window
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;    
+    
+    let resizer = SCALER*
+        Math.min(
+            window.innerWidth / VIDEO.videoWidth,
+            window.innerHeight / VIDEO.videoHeight
+        );
+// Size the incoming image to an optimal size, centered on the canvas, with room to move pieces            
+    SIZE.width = resizer * VIDEO.videoWidth;
+    SIZE.height = resizer * VIDEO.videoHeight;
+    SIZE.x = window.innerWidth/2 - SIZE.width/2;
+    SIZE.y = window.innerHeight/2 - SIZE.height/2;
 }
 
 function updateCanvas(){
