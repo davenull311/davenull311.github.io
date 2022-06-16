b78c5aa6fdabac7dfc8149caef6c98281e1a29e3
Dave <davenull311@gmail.com>
2022-06-16 14:25:43 -0400

    Portfolio updates

--- PortfolioSplash/portfolio.js
+++ PortfolioSplash/portfolio.js
@@ -1,17 +1,17 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {navSticky()};
+window.onscroll = function () { navSticky() }
 
 // Get the navbar
-var navbar = document.getElementById("nav-bar");
+const navbar = document.getElementById('nav-bar')
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+const sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navSticky() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add('sticky')
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove('sticky')
   }
 }
