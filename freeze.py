438b97085ec4326b3a9f65a39af102e316b501a2
Dave Null <davenull311@gmail.com>
2025-02-25 00:21:33 -0500

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,11 +1,12 @@
 from flask_frozen import Freezer
 from app import create_app
+import os
 
 app = create_app()
 freezer = Freezer(app)
 
-# Set the destination explicitly to the root build directory
-freezer.destination = 'build'
+# Set the destination to the absolute path of the root build directory
+freezer.destination = os.path.abspath('build')
 
 # Debug: Print registered routes
 with app.app_context():
